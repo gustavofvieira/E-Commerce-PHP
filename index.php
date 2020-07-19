9c97aeac1bc5e99e2ec12213bfa53a6ce13be8d3
Gustavo Ferreira <gustavofvieira@live.com>
2020-07-19 19:46:59 -0300

    subindo template

--- index.php
+++ index.php
@@ -1,13 +1,45 @@
 <?php
 
 require_once("vendor/autoload.php");
+require_once("vendor/hcodebr/php-classes/src/DB/Sql.php");
 
+use \Slim\Slim;
+use \Hcode\Page;
+
+//  $sql = new Sql();
+//  $results = $sql->select("SELECT * FROM tb_users");
+//  echo json_encode($results);
 $app = new \Slim\Slim();
+
 
 $app->config('debug',true);
 
 $app->get('/', function(){
-    echo "Ok";
+  
+    $page = new Page();
+    $page->setTpl("index");
+   
+     //$sql = new Sql();
+     //$results = $sql->select("SELECT * FROM tb_users");
+     //echo json_encode($results);
 });
 
 $app->run();
+
+
+
+
+
+
+// $app = new \Slim\Slim();
+
+// $app->config('debug',true);
+
+// $app->get('/', function(){
+//    $sql = new Hcode\DB\Sql();
+//    $results = $sql->select("SELECT * FROM tb_users");
+//    echo json_encode($results);
+//    // echo "Ok";
+// });
+
+// $app->run();
