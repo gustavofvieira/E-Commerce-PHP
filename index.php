7e8425c761187f0af0c6c6955780d9fabad31642
Gustavo Ferreira <gustavofvieira@live.com>
2020-07-19 21:47:18 -0300

    subindo template admin

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require_once("vendor/hcodebr/php-classes/src/DB/Sql.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 //  $sql = new Sql();
 //  $results = $sql->select("SELECT * FROM tb_users");
@@ -17,6 +18,16 @@ $app->config('debug',true);
 $app->get('/', function(){
   
     $page = new Page();
+    $page->setTpl("index");
+   
+     //$sql = new Sql();
+     //$results = $sql->select("SELECT * FROM tb_users");
+     //echo json_encode($results);
+});
+
+$app->get('/admin', function(){
+  
+    $page = new PageAdmin();
     $page->setTpl("index");
    
      //$sql = new Sql();
